7914f431a729262f2850606f7cc01d798190c9bf
Drew Devereux <drew.devereux@csiro.au>
2020-03-25 04:51:44 -0400

    Remove releaseResource command which is already inherited

--- src/ska/mccs/MccsSubarray.py
+++ src/ska/mccs/MccsSubarray.py
@@ -173,43 +173,16 @@ class MccsSubarray(SKASubarray):
         )
 
     @command(
-        dtype_out="DevString",
-        doc_out="ASCII string that indicates status, for information purposes only",  # noqa: E501
-    )
-    @DebugIt()
-    def releaseResources(self):
-
-        """Cause the subarray to release all Stations, Tiles and Station Beams,
-        and transition to idle. The released Stations, Tiles and Station Beams
-        are returned to the pool of the unassigned resources.
-
-        :todo: This method is a stub that does nothing but return a dummy
-               string.
-        :return: ASCII String that indicates status, for information purposes
-                 only
-        :rtype: DevString
-        """
-        return (
-            "Dummy ASCII string returned from "
-            "MccsSubarray.releaseResources() to indicate status, for "
-            "information purposes only"
-        )
-
-    @command(
-        dtype_in="DevVarLongArray",
-        doc_in="Specification of the segment of the transient buffer to send, "
-        "comprising:"
-        "1. Start time (timestamp: milliseconds since UNIX epoch)"
-        "2. End time (timestamp: milliseconds since UNIX epoch)"
-        "3. Dispersion measure"
-        "Together, these parameters narrow the selection of transient buffer "
-        "data to the period of time and frequencies that are of interest."
-        ""
-        "Additional metadata, such as the ID of a triggering Scheduling "
-        "Block, may need to be supplied to allow SDP to assign data ownership "
-        "correctly (TBD75).",
-        dtype_out="DevString",
-        doc_out="ASCII string that indicates status, for information purposes only",  # noqa: E501
+        dtype_in='DevVarLongArray',
+        doc_in="Specification of the segment of the transient buffer to send, comprising:"
+               "1. Start time (timestamp: milliseconds since UNIX epoch)"
+               "2. End time (timestamp: milliseconds since UNIX epoch)"
+               "3. Dispersion measure"
+               "Together, these parameters narrow the selection of transient buffer data to the period of time and frequencies that are of interest."
+               ""
+               "Additional metadata, such as the ID of a triggering Scheduling Block, may need to be supplied to allow SDP to assign data ownership correctly (TBD75).",
+        dtype_out='DevString',
+        doc_out="ASCII string that indicates status, for information purposes only",
     )
     @DebugIt()
     def sendTransientBuffer(self, argin):

77c7534db30b0c1a31799305c19cba88f0bb65d8
Malte Marquarding <malte.marquarding@csiro.au>
2020-05-27 22:43:51 +0000

    fixes to prepare for PI6 demo

--- integration_tests/test_master_subarray.py
+++ integration_tests/test_master_subarray.py
@@ -6,7 +6,14 @@ Basic order of proceedings for an iTango demo in Malte's k8s environment
 # Test master enabling and disabling subarray
 master = DeviceProxy("low/elt/master")
 subarray = DeviceProxy("low/elt/subarray_1")
-subarray.adminMode  # subarray is offline
+station_1 = DeviceProxy("low/elt/station_1")
+station_2 = DeviceProxy("low/elt/station_2")
+tile_1 = DeviceProxy("low/elt/tile_1")
+tile_2 = DeviceProxy("low/elt/tile_2")
+tile_3 = DeviceProxy("low/elt/tile_3")
+tile_4 = DeviceProxy("low/elt/tile_4")
+
+subarray_1.adminMode  # subarray is offline
 
 master.DisableSubarray(1)  # error: subarray is already disabled
 
@@ -20,11 +27,24 @@ subarray.adminMode  # subarray is offline
 
 
 # Test master allocating and releasing resources
-master.Allocate('{"subarray_id": 1, "stations": ["low/elt/station_1", "low/elt.station_2"]}')  # error: cannot allocate resources to disabled subarray
+master.Allocate(
+    '{"subarray_id": 1, "stations": ["low/elt/station_1", "low/elt/station_2"]}'
+)  # error: cannot allocate resources to disabled subarray
 
 master.EnableSubarray(1)
-master.Allocate('{"subarray_id": 1, "stations": ["low/elt/station_1", "low/elt.station_2"]}')  # error: cannot allocate resources to disabled subarray
-subarray.stationFQDNs  # ('low/elt/station_1', 'low/elt/station_2')
+
+master.Allocate(
+    '{"subarray_id": 1, "stations": ["low/elt/station_1"]}'
+)
+
+subarray.stationFQDNs  # ('low/elt/station_1')
+station_1.subarray_id # 1 - this is allocated to subarray 1
+tile_1.subarray_id # 1 - this is attached to station 1, so allocated to subarray 1
+tile_2.subarray_id # 1 - this is attached to station 1, so allocated to subarray 1
+
+station_2.subarray_id # 0 - this is unallocated
+tile_1.subarray_id # 1 - this is attached to station 2, so unallocated
+tile_2.subarray_id # 1 - this is attached to station 2, so unallocated
 
 master.Release(1)
 subarray.stationFQDNs  # empty

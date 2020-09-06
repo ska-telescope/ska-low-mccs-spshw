3ed8ce013339288d07c473feb39a651e17a80ada
Geoffrey Mant <geoffrey.mant@stfc.ac.uk>
2020-09-07 00:23:11 +0100

    add more tests

--- tests/unit/test_MccsTpmDeviceSimulator.py
+++ tests/unit/test_MccsTpmDeviceSimulator.py
@@ -36,27 +36,37 @@ class TestMccsTpmDeviceSimulator(object):
         assert device_under_test.voltage == 4.7
         device_under_test.voltage = 4.6
         assert device_under_test.voltage == 4.6
+        device_under_test.simulate = True
+        assert device_under_test.voltage != 4.6
 
     def test_current(self, device_under_test):
         """Test for the current attribute."""
         device_under_test.current == 0.4
-        device_under_test.current = 3.0
-        assert device_under_test.current == 3.0
+        device_under_test.current = 0.2
+        assert device_under_test.current == 0.2
+        device_under_test.simulate = True
+        assert device_under_test.current != 0.2
 
     def test_temperature(self, device_under_test):
         """Test for the board_temperature attribute."""
         assert device_under_test.temperature == 36.0
         device_under_test.temperature = 37.0
         assert device_under_test.temperature == 37.0
+        device_under_test.simulate = True
+        assert device_under_test.temperature != 37.0
 
     def test_fpga1_temperature(self, device_under_test):
         """Test for the fpga1_temperature attribute."""
         assert device_under_test.fpga1_temperature == 38.0
         device_under_test.fpga1_temperature = 39.0
         assert device_under_test.fpga1_temperature == 39.0
+        device_under_test.simulate = True
+        assert device_under_test.fpga1_temperature != 39.0
 
     def test_fpga2_temperature(self, device_under_test):
         """Test for the fpga2_temperature attribute."""
         assert device_under_test.fpga2_temperature == 37.5
         device_under_test.fpga2_temperature = 40.0
         assert device_under_test.fpga2_temperature == 40.0
+        device_under_test.simulate = True
+        assert device_under_test.fpga2_temperature != 40.0

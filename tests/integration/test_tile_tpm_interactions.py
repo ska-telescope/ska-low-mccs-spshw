4a1a99fcdde83daad74a9ed36f88fde4f61bef5d
Drew Devereux <drew.devereux@csiro.au>
2020-09-07 09:05:08 +0000

    [MCCS-81] Still tweaking

--- tests/integration/test_tile_tpm_interactions.py
+++ tests/integration/test_tile_tpm_interactions.py
@@ -3,6 +3,7 @@ This module contains tests of MccsTile that requires the presence of a
 MccsTpmDeviceSimulator interactions between ska.low.mccs classes,
 particularly tango devices.
 """
+import time
 from ska.low.mccs import MccsTile, MccsTpmDeviceSimulator
 
 
@@ -26,29 +27,34 @@ class TestMccsTile_MccsTpmDevicewSimulator_Integration:
     def test_voltage(self, device_context):
         """Test for the voltage attribute."""
         tile = device_context.get_device("low/elt/tile_1")
-        tile.Connect(False)
+        tile.Connect(True)
+        time.sleep(1)
         assert tile.voltage == 4.7
 
     def test_current(self, device_context):
         """Test for the current attribute."""
         tile = device_context.get_device("low/elt/tile_1")
-        tile.Connect(False)
+        tile.Connect(True)
+        time.sleep(1)
         tile.current == 0.4
 
     def test_board_temperature(self, device_context):
         """Test for the board_temperature attribute."""
         tile = device_context.get_device("low/elt/tile_1")
-        tile.Connect(False)
+        tile.Connect(True)
+        time.sleep(1)
         assert tile.board_temperature == 36.0
 
     def test_fpga1_temperature(self, device_context):
         """Test for the fpga1_temperature attribute."""
         tile = device_context.get_device("low/elt/tile_1")
-        tile.Connect(False)
+        tile.Connect(True)
+        time.sleep(1)
         assert tile.fpga1_temperature == 38.0
 
     def test_fpga2_temperature(self, device_context):
         """Test for the fpga2_temperature attribute."""
         tile = device_context.get_device("low/elt/tile_1")
-        tile.Connect(False)
+        tile.Connect(True)
+        time.sleep(1)
         assert tile.fpga2_temperature == 37.5

43d8fc26caf8810d33a49fee16be59238dae46c4
Ross Lipscomb <ross.lipscomb@itdev.co.uk>
2020-10-07 14:56:32 +0000

    MCCS-199 restore removed files

---
+++ scripts/tear_down_tango_grafana.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo Tear down Tango Grafana elements
+cd skampi
+make delete HELM_CHART=archiver VALUES=../enable_archiver.yaml
+cd ../..
+make delete
+cd scripts/TANGO-grafana/
+make uninstall-chart
+cd ../skampi
+make delete_traefik
+make delete HELM_CHART=tango-base
+cd ..
+echo Remove TANGO-grafana and skampi clones
+rm -rf TANGO-grafana
+rm -rf skampi
+

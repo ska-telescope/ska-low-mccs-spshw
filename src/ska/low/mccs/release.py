ac66225429c7180894a54ccd4383dc8875a62501
Malte Marquarding <Malte.Marquarding@csiro.au>
2020-10-30 15:09:44 +1100

    apply release 0.3.0

--- src/ska/low/mccs/release.py
+++ src/ska/low/mccs/release.py
@@ -8,7 +8,7 @@
 import sys
 
 name = """ska.low.mccs"""
-version = "0.2.1"
+version = "0.3.0"
 version_info = version.split(".")
 description = """A set of Low MCCS tango devices for the SKA Telescope."""
 author = "Team MCCS"

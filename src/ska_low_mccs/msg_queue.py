869cd4144e469b31736cb46f67510bb716381b64
Ross Lipscomb <ross.lipscomb@itdev.co.uk>
2021-04-16 08:29:32 +0000

    MCCS-510 checkout msg_queue from MCCS-511 branch

--- src/ska_low_mccs/msg_queue.py
+++ src/ska_low_mccs/msg_queue.py
@@ -5,22 +5,22 @@
 # Distributed under the terms of the GPL license.
 # See LICENSE.txt for more info.
 """
-This module implements a message queue that executed message in it's own
-thread.
+This module implements a message queue that executes messages (in a
+serial fashion) in its own thread.
 """
 import threading
 import json
-from json import JSONEncoder
-from tango import EnsureOmniThread, DeviceProxy
-from queue import Queue
+from uuid import uuid4
+import tango
+from tango import EnsureOmniThread
+from queue import SimpleQueue, Empty
 from ska_tango_base.commands import ResultCode
-import uuid
 
 
 class MessageQueue(threading.Thread):
     """
     A class for managing a queue of messages and the serial execution of
-    said messages in their own thread.
+    said messages in the queue's execution thread.
 
     A Tango device can use this message queue mechanism to run in a
     separate thread. This thread will monitor and execute messages
@@ -40,7 +40,6 @@ class MessageQueue(threading.Thread):
             command: str,
             argin: str,
             msg_uid: str,
-            timeout: int,
             notifications: bool,
             respond_to_fqdn: str,
             callback: str,
@@ -51,7 +50,6 @@ class MessageQueue(threading.Thread):
             :param command: Tango command
             :param argin: Optional argument to send to the command
             :param msg_uid: The message's unique identifier
-            :param timeout: Length of time before message should be cancelled
             :param notifications: Does the client require push notifications?
             :param respond_to_fqdn: Response message FQDN
             :param callback: The callback (command) to call when command is complete
@@ -59,25 +57,9 @@ class MessageQueue(threading.Thread):
             self.command = command
             self.argin = argin
             self.msg_uid = msg_uid
-            self.timeout = timeout
             self.notifications = notifications
             self.respond_to_fqdn = respond_to_fqdn
             self.callback = callback
-
-    class GenericObjectEncoder(JSONEncoder):
-        """
-        A custom encoder so we can serialise generic objects.
-        """
-
-        def default(self, obj):
-            """
-            Routine to return the object's dictionary.
-
-            :param obj: the object to serialise
-            :return: the object's dictionary
-            :rtype: dict
-            """
-            return obj.__dict__
 
     def __init__(self, target, lock, logger=None):
         """
@@ -88,7 +70,7 @@ class MessageQueue(threading.Thread):
         :param logger: the logger to be used by this object.
         """
         threading.Thread.__init__(self)
-        self._msg_queue = Queue()
+        self._msg_queue = SimpleQueue()
         self._terminate = False
         self._target = target
         self._qdebuglock = lock
@@ -112,9 +94,7 @@ class MessageQueue(threading.Thread):
         # #using-clients-with-multithreading
         with EnsureOmniThread():
             self._qdebug("msgQRunning")
-            while True:
-                if self._terminate:
-                    break
+            while not self._terminate:
                 self._check_msg_queue()
 
     def _notify_listener(self, command, progress):
@@ -124,9 +104,9 @@ class MessageQueue(threading.Thread):
 
         :param command: The command that needs a push notification
         :param progress: The notification to send to any subscribed listeners
-        :raises NotImplementedError: if a derived class doesn't implement this method
         """
-        raise NotImplementedError
+        self._qdebug("Error(_notify_listener)")
+        self._logger.error("Derived class should implement _notify_listener()")
 
     def _execute_msg(self, msg):
         """
@@ -134,21 +114,35 @@ class MessageQueue(threading.Thread):
 
         :param msg: The message to execute
         """
-        self._logger.info(f"_execute_msg{msg.msg_uid}")
-        self._qdebug(f"Exe({msg.msg_uid})")
         try:
+            # Check we have a device to respond to before executing a command
+            response_device = None
+            if msg.respond_to_fqdn:
+                response_device = tango.DeviceProxy(msg.respond_to_fqdn)
+                if not response_device:
+                    self._qdebug(f"Response device {msg.respond_to_fqdn} not found")
+                    return
+
+            self._logger.info(f"_execute_msg{msg.msg_uid}")
+            self._qdebug(f"Exe({msg.msg_uid})")
             command = self._target.get_command_object(msg.command)
             if command:
                 if msg.notifications:
                     self._qdebug(f"^({msg.msg_uid})")
                     self._notify_listener(msg.command, ResultCode.STARTED)
 
-                # Incorporate fqdn and callback into args dictionary
+                # Incorporate FQDN and callback into args dictionary
                 # Add to argin and deal with the case if it's not a JSON encoded string
                 try:
                     kwargs = json.loads(msg.argin)
                 except ValueError:
                     kwargs = {}
+                except TypeError:
+                    self._qdebug(f"TypeError({msg.command})")
+                    self._logger.error(
+                        f"TypeError: argin for {msg.command} should be a JSON encoded string"
+                    )
+                    return
                 kwargs["respond_to_fqdn"] = msg.respond_to_fqdn
                 kwargs["callback"] = msg.callback
                 json_string = json.dumps(kwargs)
@@ -169,19 +163,18 @@ class MessageQueue(threading.Thread):
             self._logger.error(message)
             return
 
-        # Determine if a response message is required
-        if msg.respond_to_fqdn:
+        # Determine if we need to respond to a device
+        if response_device:
             response = {
                 "msg_obj": msg,
                 "result_code": result_code,
                 "message": message,
             }
-            device = DeviceProxy(msg.respond_to_fqdn)
-            self._qdebug(f'Reply to {device}.command_inout("{msg.callback}")')
-
-            # Call device method to post a message
-            json_string = json.dumps(response, cls=self.GenericObjectEncoder)
-            device.command_inout(msg.callback, json_string)
+            # Custom JSON encode required due to msg object embedded in our response
+            json_string = json.dumps(response, default=lambda obj: obj.__dict__)
+            self._qdebug(f'Reply to {response_device}.command_inout("{msg.callback}")')
+            # Post response message
+            response_device.command_inout(msg.callback, json_string)
             self._qdebug("Reply msg sent")
         else:
             self._qdebug("No reply required")
@@ -190,11 +183,11 @@ class MessageQueue(threading.Thread):
     def _check_msg_queue(self):
         """
         Check to see if a message is waiting to be executed.
-
-        Note: This method blocks if the message queue is empty
         """
-        self._logger.info("Waiting for message to execute")
-        msg = self._msg_queue.get()
+        try:
+            msg = self._msg_queue.get_nowait()
+        except Empty:
+            return
         self._execute_msg(msg)
 
     def terminate_thread(self):
@@ -207,7 +200,6 @@ class MessageQueue(threading.Thread):
         self,
         command: str,
         argin: str = "",
-        timeout: int = 60,
         notifications: bool = False,
         respond_to_fqdn: str = "",
         callback: str = "",
@@ -219,7 +211,6 @@ class MessageQueue(threading.Thread):
 
         :param command: Command to add to Tango device's message queue
         :param argin: Optional argument to send to the command
-        :param timeout: Maximum duration that client will wait for completion of command
         :param notifications: Client requirement for push notifications
             for command's result attribute
         :param respond_to_fqdn: Response message FQDN
@@ -229,12 +220,11 @@ class MessageQueue(threading.Thread):
             a message string indicating status and a message object
         :rtype: (ResultCode, str, Message)
         """
-        msg_uid = f"{str(uuid.uuid4())}:{command}"
+        msg_uid = f"{str(uuid4())}:{command}"
         msg = self.Message(
             command=command,
             argin=argin,
             msg_uid=msg_uid,
-            timeout=timeout,
             notifications=notifications,
             respond_to_fqdn=respond_to_fqdn,
             callback=callback,
@@ -254,7 +244,6 @@ class MessageQueue(threading.Thread):
         respond_to_fqdn: str,
         callback: str,
         argin: str = "",
-        timeout: int = 60,
         notifications: bool = False,
     ):
         """
@@ -266,7 +255,6 @@ class MessageQueue(threading.Thread):
         :param respond_to_fqdn: Response message FQDN
         :param callback: The callback (command) to call when command is complete
         :param argin: Optional argument to send to the command
-        :param timeout: Maximum duration that client will wait for completion of command
         :param notifications: Client requirement for push notifications for
             command's result attribute
         :return: A tuple containing a result code (QUEUED, ERROR),
@@ -276,7 +264,6 @@ class MessageQueue(threading.Thread):
         return self.send_message(
             command=command,
             argin=argin,
-            timeout=timeout,
             notifications=notifications,
             respond_to_fqdn=respond_to_fqdn,
             callback=callback,

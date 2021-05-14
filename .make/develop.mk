906b7eb7729a652fadb941a5ec6eab278b4608ea
Drew Devereux <drew.devereux@csiro.au>
2021-05-14 03:56:23 +0000

    [MCCS-550] Get permissions right

--- .make/develop.mk
+++ .make/develop.mk
@@ -2,31 +2,31 @@ ifneq ($(REBUILD_TOX),)
 REBUILD_ARG := -r
 endif
 
+UID := $(shell id -u)
+GID := $(shell id -g)
+
 develop:
-	@docker build -t ska_low_mccs_tester . -f develop.Dockerfile
+	@docker build --build-arg UID=${UID} --build-arg GID=${GID} \
+	-t ska_low_mccs_tester . -f develop.Dockerfile
 	@docker run --rm -ti -v $(PWD):/app \
-	--user $(id -u):$(id -g) \
-	--env TOX_WORK_DIR=.tox.make \
 	ska_low_mccs_tester bash
 
 tests:
-	@docker build -t ska_low_mccs_tester . -f develop.Dockerfile
+	@docker build --build-arg UID=${UID} --build-arg GID=${GID} \
+	-t ska_low_mccs_tester . -f develop.Dockerfile
 	@docker run --rm -v $(PWD):/app \
-	--user $(id -u):$(id -g) \
-	--env TOX_WORK_DIR=.tox.make \
 	ska_low_mccs_tester tox ${REBUILD_ARG} -e py
 
 lint:
-	@docker build -t ska_low_mccs_tester . -f develop.Dockerfile
+	@docker build --build-arg UID=${UID} --build-arg GID=${GID} \
+	-t ska_low_mccs_tester . -f develop.Dockerfile
 	@docker run --rm -v $(PWD):/app \
-	--user $(id -u):$(id -g) \
-	--env TOX_WORK_DIR=.tox.make \
 	ska_low_mccs_tester tox ${REBUILD_ARG} -e lint
 
 docs:
 	@docker build -t ska_low_mccs_docs_builder  . -f docs/Dockerfile
-	@docker run --rm -v $(PWD):/project --user $(id -u):$(id -g) ska_low_mccs_docs_builder
+	@docker run --rm -v $(PWD):/project --user ${UID}:${GID} ska_low_mccs_docs_builder
 
 testdocs:
 	@docker build -t ska_low_mccs_testdocs_builder --build-arg dir=testing/docs . -f docs/Dockerfile 
-	@docker run --rm -v $(PWD):/project --user $(id -u):$(id -g) ska_low_mccs_testdocs_builder
+	@docker run --rm -v $(PWD):/project --user ${UID}:${GID} ska_low_mccs_testdocs_builder

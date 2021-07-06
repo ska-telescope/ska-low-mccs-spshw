a349f4a33312973c3f14a06f3e9a2799a34d28bc
Drew Devereux <drew.devereux@csiro.au>
2021-07-06 01:46:15 +0000

    [MCCS-401] Transient buffer

---
+++ src/ska_low_mccs/transient_buffer/transient_buffer_device.py
@@ -0,0 +1,220 @@
+# type: ignore
+# -*- coding: utf-8 -*-
+#
+# This file is part of the SKA Low MCCS project
+#
+#
+#
+# Distributed under the terms of the GPL license.
+# See LICENSE.txt for more info.
+
+"""This module implements the MCCS transient buffer device."""
+from __future__ import annotations
+
+from tango.server import attribute
+
+from ska_tango_base.base import SKABaseDevice
+from ska_tango_base.commands import ResultCode
+from ska_tango_base.control_model import HealthState
+
+from ska_low_mccs import release
+from ska_low_mccs.component import CommunicationStatus
+from ska_low_mccs.transient_buffer import (
+    TransientBufferComponentManager,
+    TransientBufferHealthModel,
+)
+
+__all__ = ["MccsTransientBuffer", "main"]
+
+
+class MccsTransientBuffer(SKABaseDevice):
+    """An implementation of a transient buffer Tango device for MCCS."""
+
+    # ---------------
+    # Initialisation
+    # ---------------
+    def _init_state_model(self: MccsTransientBuffer) -> None:
+        super()._init_state_model()
+        self._health_state = HealthState.UNKNOWN  # InitCommand.do() does this too late.
+        self._health_model = TransientBufferHealthModel(self.health_changed)
+        self.set_change_event("healthState", True, False)
+
+    def create_component_manager(
+        self: MccsTransientBuffer,
+    ) -> TransientBufferComponentManager:
+        """
+        Create and return a component manager for this device.
+
+        :return: a component manager for this device.
+        """
+        return TransientBufferComponentManager(
+            self.logger,
+            self._component_communication_status_changed,
+        )
+
+    class InitCommand(SKABaseDevice.InitCommand):
+        """
+        A class for :py:class:`~.MccsTransientBuffer`'s Init command.
+
+        The :py:meth:`~.MccsTransientBuffer.InitCommand.do` method below
+        is called upon :py:class:`~.MccsTransientBuffer`'s
+        initialisation.
+        """
+
+        def do(self: MccsTransientBuffer.InitCommand) -> tuple[ResultCode, str]:
+            """
+            Initialises the attributes and properties of the
+            :py:class:`.MccsTransientBuffer`.
+
+            :return: A tuple containing a return code and a string
+                message indicating status. The message is for
+                information purpose only.
+            """
+            device = self.target
+
+            device._build_state = release.get_release_info()
+            device._version_id = release.version
+
+            # The health model updates our health, but then the base class super().do()
+            # overwrites it with OK, so we need to update this again.
+            # TODO: This needs to be fixed in the base classes.
+            device._health_state = device._health_model.health_state
+
+            return super().do()
+
+    # ----------
+    # Callbacks
+    # ----------
+    def _component_communication_status_changed(
+        self: MccsTransientBuffer,
+        communication_status: CommunicationStatus,
+    ) -> None:
+        """
+        Handle change in communications status between component manager and component.
+
+        This is a callback hook, called by the component manager when
+        the communications status changes. It is implemented here to
+        drive the op_state.
+
+        :param communication_status: the status of communications
+            between the component manager and its component.
+        """
+        action_map = {
+            CommunicationStatus.DISABLED: "component_disconnected",
+            CommunicationStatus.NOT_ESTABLISHED: "component_unknown",
+            CommunicationStatus.ESTABLISHED: None,  # wait for a power mode update
+        }
+
+        action = action_map[communication_status]
+        if action is not None:
+            self.op_state_model.perform_action(action)
+
+        self._health_model.is_communicating(
+            communication_status == CommunicationStatus.ESTABLISHED
+        )
+
+    def health_changed(self, health):
+        """
+        Handle change in this device's health state.
+
+        This is a callback hook, called whenever the HealthModel's
+        evaluated health state changes. It is responsible for updating
+        the tango side of things i.e. making sure the attribute is up to
+        date, and events are pushed.
+
+        :param health: the new health value
+        :type health: :py:class:`~ska_tango_base.control_model.HealthState`
+        """
+        if self._health_state == health:
+            return
+        self._health_state = health
+        self.push_change_event("healthState", health)
+
+    # ----------
+    # Attributes
+    # ----------
+    @attribute(dtype="DevString", label="stationId")
+    def stationId(self):
+        """
+        Return the station id.
+
+        :return: the station id
+        :rtype: int
+        """
+        return self.component_manager.station_id
+
+    @attribute(dtype="DevString", label="transientBufferJobId")
+    def transientBufferJobId(self):
+        """
+        Return the transient buffer job id.
+
+        :return: the transient buffer job id
+        :rtype: int
+        """
+        return self.component_manager.transient_buffer_job_id
+
+    @attribute(dtype="DevLong", label="resamplingBits")
+    def resamplingBits(self):
+        """
+        Return the resampling bit depth.
+
+        :return: the resampling bit depth
+        :rtype: int
+        """
+        return self.component_manager.resampling_bits
+
+    @attribute(dtype="DevShort", label="nStations")
+    def nStations(self):
+        """
+        Return the number of stations.
+
+        :return: the number of stations
+        :rtype: int
+        """
+        return self.component_manager.n_stations
+
+    @attribute(
+        dtype=("DevDouble",),
+        max_dim_x=100,
+        label="transientFrequencyWindow",
+    )
+    def transientFrequencyWindow(self):
+        """
+        Return the transient frequency window.
+
+        :return: the transient frequency window
+        :rtype: list(float)
+        """
+        return self.component_manager.transient_frequency_window
+
+    @attribute(dtype=("DevString",), max_dim_x=100, label="stationIds")
+    def stationIds(self):
+        """
+        Return the station ids.
+
+        :return: the station ids
+        :rtype: list(str)
+        """
+        return self.component_manager.station_ids
+
+
+# ----------
+# Run server
+# ----------
+def main(args=None, **kwargs):
+    """
+    Entry point for module.
+
+    :param args: positional arguments
+    :type args: list
+    :param kwargs: named arguments
+    :type kwargs: dict
+
+    :return: exit code
+    :rtype: int
+    """
+    return MccsTransientBuffer.run_server(args=args, **kwargs)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/ska_low_mccs/transient_buffer/transient_buffer_health_model.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the SKA Low MCCS project
+#
+#
+#
+# Distributed under the terms of the GPL license.
+# See LICENSE.txt for more info.
+
+"""An implementation of a health model for a transient buffer."""
+from ska_low_mccs.health import HealthModel
+
+
+__all__ = ["TransientBufferHealthModel"]
+
+
+class TransientBufferHealthModel(HealthModel):
+    """
+    A health model for a transient buffer.
+
+    At present this uses the base health model; this is a placeholder
+    for a future, better implementation.
+    """

51cbd19e957528a0736cfe92d0a378eecb7e3a9c
Ben Whitmore <bww@observatorysciences.co.uk>
2021-07-15 14:28:48 +0000

    [mccs-402] remove 2xsubarray beams from test

--- testing/src/testing/tests/functional/test_controller_subarray_interactions.py
+++ testing/src/testing/tests/functional/test_controller_subarray_interactions.py
@@ -172,8 +172,8 @@ def subarraybeams(tango_harness: TangoHarness):
     return {
         1: tango_harness.get_device("low-mccs/subarraybeam/01"),
         2: tango_harness.get_device("low-mccs/subarraybeam/02"),
-        3: tango_harness.get_device("low-mccs/subarraybeam/03"),
-        4: tango_harness.get_device("low-mccs/subarraybeam/04"),
+        # 3: tango_harness.get_device("low-mccs/subarraybeam/03"),
+        # 4: tango_harness.get_device("low-mccs/subarraybeam/04"),
     }
 
 

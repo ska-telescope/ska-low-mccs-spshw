39e9a4d1188877bfc7e964f3c54599c7f31cb9c0
Ross Lipscomb <ross.lipscomb@itdev.co.uk>
2021-08-06 10:56:55 +0000

    MCCS-692 Release 0.8.5

--- src/ska_low_mccs/release.py
+++ src/ska_low_mccs/release.py
@@ -10,7 +10,7 @@ from typing import Optional
 
 
 name = "ska_low_mccs"
-version = "0.8.4"
+version = "0.8.5"
 version_info = version.split(".")
 description = "A set of Low MCCS tango devices for the SKA Telescope."
 author = "Team MCCS"

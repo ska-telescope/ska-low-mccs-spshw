ed0aa66acf7016eddf0ca43eb4f6a8acbbe32078
Geoffrey Mant <geoffrey.mant@stfc.ac.uk>
2022-10-06 09:51:34 +0100

    MCCS-1134 mods to docs

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -58,8 +58,8 @@ autodoc_mock_imports = [
     "tango",
     "uritools",
     "yaml",
-    "ska-control-model"
-    "ska-low-mccs-common"
+    "ska_control_model",
+    "ska_low_mccs_common",
 ]
 
 

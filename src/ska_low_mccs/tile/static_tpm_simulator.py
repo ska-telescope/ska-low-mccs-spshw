adecedcbaaa11052f6ddb65729f1cd0253306b3d
Alistair <abc@observatorysciences.co.uk>
2022-11-07 08:54:35 +0000

    [MCCS-1096] Add StaticTpmSimulatorClass for testing tpm_driver

--- src/ska_low_mccs/tile/static_tpm_simulator.py
+++ src/ska_low_mccs/tile/static_tpm_simulator.py
@@ -81,3 +81,159 @@ class StaticTpmSimulator(BaseTpmSimulator):
         :return: the temperature of FPGA 2
         """
         return self._fpga2_temperature
+        
+class StaticTpmDriverSimulator(StaticTpmSimulator):
+    """A simulator for a TPM."""
+
+
+    def __init__(self: StaticTpmSimulator, logger: logging.Logger) -> None:
+        """
+        Initialise a new TPM simulator instance.
+
+        :param logger: a logger for this simulator to use
+        """
+        self.memory_map= {}
+        self.tpm = False
+        super().__init__(logger)
+
+    def get_fpga0_temperature(self):
+        return self.fpga1_temperature
+
+    def get_fpga1_temperature(self):
+        return self.fpga2_temperature
+    def get_temperature(self):
+        return self.board_temperature
+    def get_voltage(self):
+        return self.voltage
+    def get_tile_id(self):
+        print("getting tile id")
+        return self.tile_id
+    def get_firmware_list(self):
+        return
+    def program_fpgas(self):
+        return 
+    def set_station_id(self):
+        return 
+    def get_adc_rms(self):
+        return self.adc_rms
+
+    def get_fpga_time(self):
+        return 
+    def get_adc_rms(self):
+        return self.adc_rms
+    def get_adc_rms(self):
+        return self.adc_rms
+    def get_adc_rms(self):
+        return self.adc_rms
+    def get_40g_core_configuration(
+        self: BaseTpmSimulator,
+        core_id: int = -1,
+        arp_table_entry: int = 0,
+    ) -> dict | list[dict] | None:
+        """
+        Return a 40G configuration.
+
+        :param core_id: id of the core for which a configuration is to
+            be return. Defaults to -1, in which case all cores
+            configurations are returned, defaults to -1
+        :param arp_table_entry: ARP table entry to use
+
+        :return: core configuration or list of core configurations
+        """
+        if core_id == -1:
+            return self._forty_gb_core_list
+        for item in self._forty_gb_core_list:
+            if item.get("core_id") == core_id:
+                print(item)
+                return item
+        return 
+
+    def read_address(self: BaseTpmSimulator, address: int) -> list[int]:
+        """
+        Return a list of values from a given address.
+
+        :param address: address of start of read
+        :param nvalues: number of values to read
+
+        :return: values at the address
+        """
+
+        return self._address_map.get(str(address), 0)
+
+    def write_address(self: BaseTpmSimulator, address: int, values: int) -> None:
+        """
+        Write a list of values to a given address.
+
+        :param address: address of start of read
+        :param values: values to write
+        """
+
+        self._address_map.update({str(address): values})
+
+    def connect(self):
+        self[int(0x30000000)] = [3,7]
+        self.tpm = {""}
+
+    def __getitem__(self, key):
+                # Check if the specified key is a memory address or register name
+        if isinstance(key, int):
+            return self.read_address(key)
+
+        # Check if the specified key is a tuple, in which case we are reading from a device
+        if type(key) is tuple:
+            # Run checks
+            if not self._checks():
+                return
+
+            if len(key) == 2:
+                return self.read_device(key[0], key[1])
+            else:
+                raise LibraryError("A device name and address need to be specified for writing to SPI devices")
+
+        elif type(key) is str or isinstance(key, basestring):
+            # Run checks
+            if not self._checks():
+                return
+
+            # Check if a device is specified in the register name
+            if self.memory_map.has_register(key):
+                reg = self.memory_map[key]
+                return self.read_register(key, reg.size)
+        else:
+            raise LibraryError("Unrecognised key type, must be register name, memory address or SPI device tuple")
+
+        # Register not found
+        raise LibraryError("Register %s not found" % key)
+
+        
+    def __setitem__(self, key, value):
+                # Check is the specified key is a memory address or register name
+        if isinstance(key, int):
+            print("dsd")
+            return self.write_address(key, value)
+
+        # Check if the specified key is a tuple, in which case we are writing to a device
+        if type(key) is tuple:
+            # Run checks
+            if not self._checks():
+                return
+
+            if len(key) == 2:
+                return self.write_device(key[0], key[1], value)
+            else:
+                raise LibraryError("A device name and address need to be specified for writing to SPI devices")
+
+        elif type(key) is str or isinstance(key, basestring):
+            # Run checks
+            if not self._checks():
+                return
+
+            # Check if device is specified in the register name
+            if self.memory_map.has_register(key):
+                return self.write_register(key, value)
+        else:
+            raise LibraryError(
+            	"Unrecognised key type (%s), must be register name or memory address" % key.__class__.__name__)
+
+        # Register not found
+        raise LibraryError("Register %s not found" % key)

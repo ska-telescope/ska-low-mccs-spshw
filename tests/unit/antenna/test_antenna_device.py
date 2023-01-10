7a2553e7ebe9637e052fd73a8469b4f61b000640
Joseph Harvey <joe.w.harvey@ntlworld.com>
2023-01-10 15:02:09 +0000

    fixed the antenna tests file location

--- tests/unit/antenna/test_antenna_device.py
+++ tests/unit/antenna/test_antenna_device.py
@@ -690,7 +690,7 @@ class TestPatchedAntenna:
         :return: specification of the device to be loaded
         """
         return {
-            "path": "charts/ska-low-mccs/data/configuration.json",
+            "path": "tests/data/configuration.json",
             "package": "ska_low_mccs",
             "device": "antenna_000001",
             "proxy": MccsDeviceProxy,

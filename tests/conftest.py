d3f9768de1a5833c28c237c78468dbbf2f9c3dfc
Drew Devereux <drew.devereux@data61.csiro.au>
2023-01-20 07:11:15 +0000

    [MCCS-1259] mv tile-only fixtures to tile conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,29 +15,16 @@ functional (BDD).
 """
 from __future__ import annotations
 
-import functools
 import logging
 import threading
 import time
-import unittest
-from typing import Any, Callable, Generator, Set, cast
+from typing import Any, Set, cast
 
 import _pytest
 import pytest
 import tango
 import uvicorn
 import yaml
-from ska_low_mccs_common.testing.mock import MockChangeEventCallback, MockDeviceBuilder
-from ska_low_mccs_common.testing.tango_harness import (
-    ClientProxyTangoHarness,
-    DeploymentContextTangoHarness,
-    DevicesToLoadType,
-    MccsDeviceInfo,
-    MockingTangoHarness,
-    StartingStateTangoHarness,
-    TangoHarness,
-    TestContextTangoHarness,
-)
 
 from ska_low_mccs_spshw.subrack import FanMode, SubrackData
 from ska_low_mccs_spshw.subrack.subrack_simulator import SubrackSimulator
@@ -134,181 +121,6 @@ def pytest_collection_modifyitems(
             )
 
 
-@pytest.fixture()
-def initial_mocks() -> dict[str, unittest.mock.Mock]:
-    """
-    Fixture that registers device proxy mocks prior to patching.
-
-    By default no initial mocks are registered, but this fixture can be
-    overridden by test modules/classes that need to register initial
-    mocks.
-
-    :return: an empty dictionary
-    """
-    return {}
-
-
-@pytest.fixture()
-def mock_factory() -> Callable[[], unittest.mock.Mock]:
-    """
-    Fixture that provides a mock factory for device proxy mocks.
-
-    This default factory
-    provides vanilla mocks, but this fixture can be overridden by test modules/classes
-    to provide mocks with specified behaviours.
-
-    :return: a factory for device proxy mocks
-    """
-    return MockDeviceBuilder()
-
-
-@pytest.fixture(scope="session")
-def tango_harness_factory(
-    request: pytest.FixtureRequest, logger: logging.Logger
-) -> Callable[
-    [
-        dict[str, Any],
-        DevicesToLoadType,
-        Callable[[], unittest.mock.Mock],
-        dict[str, unittest.mock.Mock],
-    ],
-    TangoHarness,
-]:
-    """
-    Return a factory for creating a test harness for testing Tango devices.
-
-    The Tango context used depends upon the context in which the tests are being
-    run, as specified by the `--testbed` option.
-
-    If the context is "test", then this harness deploys the specified
-    devices into a
-    :py:class:`tango.test_context.MultiDeviceTestContext`.
-
-    Otherwise, this harness assumes that devices are already running;
-    that is, we are testing a deployed system.
-
-    This fixture is implemented as a factory so that the actual
-    `tango_harness` fixture can vary in scope: unit tests require test
-    isolation, so will want to build a new harness every time. But
-    functional tests assume a single harness that maintains state
-    across multiple tests, so they will want to instantiate the harness
-    once and then use it for multiple tests.
-
-    :param request: A pytest object giving access to the requesting test
-        context.
-    :param logger: the logger to be used by this object.
-
-    :return: a tango harness factory
-    """
-
-    class _CPTCTangoHarness(ClientProxyTangoHarness, TestContextTangoHarness):
-        """
-        A Tango test harness.
-
-        With the client proxy functionality of
-        :py:class:`~ska_low_mccs_common.testing.tango_harness.ClientProxyTangoHarness`
-        within the lightweight test context provided by
-        :py:class:`~ska_low_mccs_common.testing.tango_harness.TestContextTangoHarness`.
-        """
-
-        pass
-
-    testbed = request.config.getoption("--testbed")
-
-    def build_harness(
-        tango_config: dict[str, Any],
-        devices_to_load: DevicesToLoadType,
-        mock_factory: Callable[[], unittest.mock.Mock],
-        initial_mocks: dict[str, unittest.mock.Mock],
-    ) -> TangoHarness:
-        """
-        Build the Tango test harness.
-
-        :param tango_config: basic configuration information for a tango
-            test harness
-        :param devices_to_load: fixture that provides a specification of the
-            devices that are to be included in the devices_info dictionary
-        :param mock_factory: the factory to be used to build mocks
-        :param initial_mocks: a pre-build dictionary of mocks to be used
-            for particular
-
-        :return: a tango test harness
-        """
-        if devices_to_load is None:
-            device_info = None
-        else:
-            device_info = MccsDeviceInfo(**devices_to_load)
-
-        tango_harness: TangoHarness  # type hint only
-        if testbed == "test":
-            tango_harness = _CPTCTangoHarness(device_info, logger, **tango_config)
-        elif testbed == "local":
-            tango_harness = DeploymentContextTangoHarness(
-                device_info, logger, **tango_config
-            )
-        else:
-            tango_harness = ClientProxyTangoHarness(device_info, logger)
-
-        starting_state_harness = StartingStateTangoHarness(tango_harness)
-
-        mocking_harness = MockingTangoHarness(
-            starting_state_harness, mock_factory, initial_mocks
-        )
-
-        return mocking_harness
-
-    return build_harness
-
-
-@pytest.fixture()
-def tango_config() -> dict[str, Any]:
-    """
-    Fixture that returns basic configuration information for a Tango test harness.
-
-    For example whether or not to run in a separate process.
-
-    :return: a dictionary of configuration key-value pairs
-    """
-    return {"process": False}
-
-
-@pytest.fixture()
-def tango_harness(
-    tango_harness_factory: Callable[
-        [
-            dict[str, Any],
-            DevicesToLoadType,
-            Callable[[], unittest.mock.Mock],
-            dict[str, unittest.mock.Mock],
-        ],
-        TangoHarness,
-    ],
-    tango_config: dict[str, str],
-    devices_to_load: DevicesToLoadType,
-    mock_factory: Callable[[], unittest.mock.Mock],
-    initial_mocks: dict[str, unittest.mock.Mock],
-) -> Generator[TangoHarness, None, None]:
-    """
-    Create a test harness for testing Tango devices.
-
-    :param tango_harness_factory: a factory that provides a test harness
-        for testing tango devices
-    :param tango_config: basic configuration information for a tango
-        test harness
-    :param devices_to_load: fixture that provides a specification of the
-        devices that are to be included in the devices_info dictionary
-    :param mock_factory: the factory to be used to build mocks
-    :param initial_mocks: a pre-build dictionary of mocks to be used
-        for particular
-
-    :yields: a tango test harness
-    """
-    with tango_harness_factory(
-        tango_config, devices_to_load, mock_factory, initial_mocks
-    ) as harness:
-        yield harness
-
-
 @pytest.fixture(scope="session")
 def logger() -> logging.Logger:
     """
@@ -319,61 +131,6 @@ def logger() -> logging.Logger:
     debug_logger = logging.getLogger()
     debug_logger.setLevel(logging.DEBUG)
     return debug_logger
-
-
-@pytest.fixture()
-def mock_callback_called_timeout() -> float:
-    """
-    Return the time to wait for a mock callback to be called when a call is expected.
-
-    This is a high value because calls will usually arrive much much
-    sooner, but we should be prepared to wait plenty of time before
-    giving up and failing a test.
-
-    :return: the time to wait for a mock callback to be called when a
-        call is asserted.
-    """
-    return 7.5
-
-
-@pytest.fixture()
-def mock_callback_not_called_timeout() -> float:
-    """
-    Return the time to wait for a mock callback to be called when a call is unexpected.
-
-    An assertion that a callback has not been called can only be passed
-    once we have waited the full timeout period without a call being
-    received. Thus, having a high value for this timeout will make such
-    assertions very slow. It is better to keep this value fairly low,
-    and accept the risk of an assertion passing prematurely.
-
-    :return: the time to wait for a mock callback to be called when a
-        call is unexpected.
-    """
-    return 0.5
-
-
-@pytest.fixture()
-def mock_change_event_callback_factory(
-    mock_callback_called_timeout: float,
-    mock_callback_not_called_timeout: float,
-) -> Callable[[str], MockChangeEventCallback]:
-    """
-    Return a factory that returns a new mock change event callback each call.
-
-    :param mock_callback_called_timeout: the time to wait for a mock
-        callback to be called when a call is expected
-    :param mock_callback_not_called_timeout: the time to wait for a mock
-        callback to be called when a call is unexpected
-
-    :return: a factory that returns a new mock change event callback
-        each time it is called with the name of a device attribute.
-    """
-    return functools.partial(
-        MockChangeEventCallback,
-        called_timeout=mock_callback_called_timeout,
-        not_called_timeout=mock_callback_not_called_timeout,
-    )
 
 
 # The following fixtures are defined here because they are needed in both

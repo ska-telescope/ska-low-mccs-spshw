6c0a01f707952b9497bafb21f34554d68d69e9b1
Giovanni Comoretto <giovanni.comoretto@inaf.it>
2023-02-23 14:37:11 +0100

    [MCCS-1346] Corrected typo

--- src/ska_low_mccs_spshw/tile/tpm_driver.py
+++ src/ska_low_mccs_spshw/tile/tpm_driver.py
@@ -23,7 +23,7 @@ import time
 from typing import Any, Callable, Optional, cast
 
 # import numpy as np
-from pyaavs.tile Tile as Tile
+from pyaavs.tile import Tile
 from pyfabil.base.definitions import Device, LibraryError
 from ska_control_model import CommunicationStatus, TaskStatus
 from ska_low_mccs_common.component import MccsBaseComponentManager

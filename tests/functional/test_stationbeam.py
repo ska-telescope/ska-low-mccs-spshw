56244d4607fe7286039e59a6deed077354b099b9
Tom Moynihan <tdm@observatorysciences.co.uk>
2023-05-23 10:35:16 +0000

    [MCCS-1497] Sync bugs

--- tests/functional/test_stationbeam.py
+++ tests/functional/test_stationbeam.py
@@ -272,8 +272,8 @@ def synchronise_the_station(
         # time.sleep(2)
     for i in range(30):
         # tm = tm + 1
-        cur_time = int(t1.ReadRegister("fpga1.pps_manager.curr_time_read_val")[0])
-        start_time = int(t1.readregister("fpga1.pps_manager.sync_time_val")[0])
+        cur_time = int(t1.ReadRegister("fpga1.pps_manager.curr_time_read_val"))
+        start_time = int(t1.readregister("fpga1.pps_manager.sync_time_val"))
         print(
             f"Current: {cur_time} - Start: {start_time} difference: "
             f"{cur_time-start_time} frame time:{t1.fpgaframetime}"

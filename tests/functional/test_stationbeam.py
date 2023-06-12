0f59a1ff8d8edfeac3255d90ca37f37712fe159f
Tom Moynihan <tdm@observatorysciences.co.uk>
2023-06-12 09:30:48 +0000

    typo

--- tests/functional/test_stationbeam.py
+++ tests/functional/test_stationbeam.py
@@ -248,7 +248,7 @@ def check_station_is_configured(station_device: tango.DeviceProxy) -> None:
     assert station_device.state() == tango.DevState.STANDBY
 
 
-@when("the station and subcracks are turned on")
+@given("the station and subcracks are turned on")
 def check_test_generator_is_programmed(
     station_device: tango.DeviceProxy,
     subrack_device: tango.DeviceProxy,

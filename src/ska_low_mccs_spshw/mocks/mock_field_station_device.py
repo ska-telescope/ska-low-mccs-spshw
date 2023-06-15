e222b21bb8da3460210dcbe1e859413742a76503
Alistair <abc@observatorysciences.co.uk>
2023-06-15 15:46:11 +0000

    [MCCS-1569] Add MockFieldStation for unit tests

---
+++ src/ska_low_mccs_spshw/mocks/mock_field_station_device.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*
+#
+# This file is part of the SKA Low MCCS project
+#
+#
+# Distributed under the terms of the BSD 3-clause new license.
+# See LICENSE for more info.
+"""This module provides a mock Tango device for a FieldStation."""
+from __future__ import annotations
+
+from typing import Any, Optional
+
+from ska_control_model import ResultCode
+from ska_tango_base.base import SKABaseDevice
+from tango.server import attribute, command
+
+from .mock_field_station_component_manager import MockFieldStationComponentManager
+
+DevVarLongStringArrayType = tuple[list[ResultCode], list[Optional[str]]]
+
+__all__ = ["MockFieldStation", "main"]
+
+
+class MockFieldStation(SKABaseDevice):
+    """An mocked implementation of the FieldStation Tango device."""
+
+    INITIAL_MOCKED_OUTSIDE_TEMPERATURE = 42.5
+
+    def create_component_manager(
+        self: MockFieldStation,
+    ) -> MockFieldStationComponentManager:
+        """
+        Create and return a mock component manager for this mock device.
+
+        :return: a component manager for this device.
+        """
+        return MockFieldStationComponentManager(
+            self.logger,
+            self._communication_state_changed,
+            self._component_state_changed,
+        )
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """
+        Initialise this mock device object.
+
+        :param args: positional args to the init
+        :param kwargs: keyword args to the init
+        """
+        super().__init__(*args, **kwargs)
+        self.outside_temperature = self.INITIAL_MOCKED_OUTSIDE_TEMPERATURE
+
+    def init_device(self: MockFieldStation) -> None:
+        """Initialise this mock device."""
+        super().init_device()
+        self.set_change_event("OutsideTemperature", True, False)
+
+    # -----------------
+    # Mocked Attributes
+    # ----------------
+    @attribute(
+        dtype="float",
+        label="outsideTemperature",
+    )
+    def outsidetemperature(self: MockFieldStation) -> float:
+        """
+        Return the mocked outside temperature.
+
+        :return: the mocked outside temperature.
+        """
+        return self.outside_temperature
+
+    # ---------------
+    # Mocked Commands
+    # ---------------
+    @command(dtype_in="float", dtype_out="DevVarLongStringArray")
+    def mockoutsidetemperaturechange(
+        self: MockFieldStation, argin: float
+    ) -> DevVarLongStringArrayType:
+        """
+        Mock a change in the outside temperature.
+
+        :param argin: A float representing the mocked outside temperature.
+        :return: A tuple containing a return code and a string message
+                indicating status.
+        """
+        self.outside_temperature = argin
+        self.push_change_event("outsidetemperature", argin)
+        return ([ResultCode.OK], ["_"])
+
+
+# ----------
+# Run server
+# ----------
+def main(*args: str, **kwargs: str) -> int:  # pragma: no cover
+    """
+    Entry point for module.
+
+    :param args: positional arguments
+    :param kwargs: named arguments
+
+    :return: exit code
+    """
+    return MockFieldStation.run_server(args=args or None, **kwargs)
+
+
+if __name__ == "__main__":
+    main()

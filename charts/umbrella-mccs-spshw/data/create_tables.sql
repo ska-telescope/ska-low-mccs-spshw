de29e892def83be1c5648ab7149dc48b09819024
Elliot Scott <ejs@observatorysciences.co.uk>
2023-06-27 16:30:33 +0000

    [MCCS-1579] postgres + pgadmin + pv

---
+++ charts/umbrella-mccs-spshw/data/create_tables.sql
@@ -0,0 +1,8 @@
+-- Script to create necessary table(s) for calibration database
+CREATE TABLE tab_mccs_calib
+(id serial NOT NULL PRIMARY KEY,
+creation_time timestamp NOT NULL,
+outside_temperature real NOT NULL,
+channel smallint NOT NULL,
+calibration real[] NOT NULL
+);

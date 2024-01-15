b53e3fd1d0ae26d0989c8b8daf4d64a3b191e401
Tom Moynihan <tdm@observatorysciences.co.uk>
2024-01-15 13:43:42 +0000

    REL-1169: bumped version to 0.12.1

--- src/ska_low_mccs_spshw/version.py
+++ src/ska_low_mccs_spshw/version.py
@@ -9,7 +9,7 @@
 
 version_info = {
     "name": "ska_low_mccs_spshw",
-    "version": "0.12.0",
+    "version": "0.12.1",
     "description": (
         "Monitoring and control of SPS hardware as part of the MCCS subsystem"
     ),

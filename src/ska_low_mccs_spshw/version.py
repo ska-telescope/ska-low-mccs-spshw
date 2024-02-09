6b47f1db69cdca421df1f2c80aea521362dbcad4
Tom Moynihan <tdm@observatorysciences.co.uk>
2024-02-09 16:24:33 +0000

    Revert "Merge branch 'rel-1222-release-v-0-14-0' into 'main'"
    This reverts merge request !173

--- src/ska_low_mccs_spshw/version.py
+++ src/ska_low_mccs_spshw/version.py
@@ -9,7 +9,7 @@
 
 version_info = {
     "name": "ska_low_mccs_spshw",
-    "version": "0.14.0",
+    "version": "0.13.0",
     "description": (
         "Monitoring and control of SPS hardware as part of the MCCS subsystem"
     ),

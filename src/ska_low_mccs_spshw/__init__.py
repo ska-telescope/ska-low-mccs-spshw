3b05caca9f14025de58112fd47657c5fdff2a2af
Tom Moynihan <tdm@observatorysciences.co.uk>
2024-02-09 13:35:34 +0000

    [REL-1222] Prepare for release

--- src/ska_low_mccs_spshw/__init__.py
+++ src/ska_low_mccs_spshw/__init__.py
@@ -12,7 +12,7 @@ The Monitoring Control and Calibration (MCCS) subsystem is responsible
 for, amongst other things, monitoring and control of LFAA.
 """
 
-__version__ = "0.13.0"
+__version__ = "0.14.0"
 __version_info__ = (
     "ska-low-mccs-spshw",
     __version__,

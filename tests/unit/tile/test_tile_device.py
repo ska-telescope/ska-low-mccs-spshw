8236e6bd4473ea9aa510e9cdeaabcb3280a15823
Alistair <abc@observatorysciences.co.uk>
2024-11-01 08:36:09 +0000

    SKB-609: make pytest approx use an absolute.

--- tests/unit/tile/test_tile_device.py
+++ tests/unit/tile/test_tile_device.py
@@ -816,7 +816,7 @@ class TestMccsTile:
         t1 = on_tile_device.read_attribute(attribute_name).time.totime()
         time.sleep(random_sleep)
         t2 = on_tile_device.read_attribute(attribute_name).time.totime()
-        assert t2 - t1 == pytest.approx(random_sleep, 0.1)
+        assert t2 - t1 == pytest.approx(random_sleep, abs=0.3)
 
     def test_tile_info(
         self: TestMccsTile,

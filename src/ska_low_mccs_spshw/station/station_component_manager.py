877acef260190bb480102a9691c26ff58a7c5596
Tom Moynihan <tdm@observatorysciences.co.uk>
2025-04-01 07:34:24 +0000

    THORN-97: Empty commit to run pipeline

--- src/ska_low_mccs_spshw/station/station_component_manager.py
+++ src/ska_low_mccs_spshw/station/station_component_manager.py
@@ -1889,7 +1889,7 @@ class SpsStationComponentManager(
             "Configuring LMC Integrated Download: "
             f"{self._lmc_integrated_ip}:{self._lmc_integrated_port}"
         )
-        # TODO: SKB-804, We have to juggle these around to route over 1G and 10G.
+        # TODO: SKB-804, We need to juggle these around to route over 1G and 10G.
         self.set_lmc_download(
             mode=self._lmc_integrated_mode,
             dst_ip=self._lmc_integrated_ip,

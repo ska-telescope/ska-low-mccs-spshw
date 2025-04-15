6ae426c19094ad1befbdbf3a3cb468a5490a27ae
Alistair <abc@observatorysciences.co.uk>
2025-04-15 12:48:25 +0000

    SKB-861: Bump minor release.

--- src/ska_low_mccs_spshw/version.py
+++ src/ska_low_mccs_spshw/version.py
@@ -9,7 +9,7 @@
 
 version_info = {
     "name": "ska_low_mccs_spshw",
-    "version": "3.0.3",
+    "version": "3.1.0",
     "description": (
         "Monitoring and control of SPS hardware as part of the MCCS subsystem"
     ),

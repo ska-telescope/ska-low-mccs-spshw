77f16f762cac45b0aba8f1cae7319444afac6d7d
George Stefan Chira <gsc@observatorysciences.co.uk>
2025-05-09 17:03:55 +0100

    THORN-11: it was that simple

--- src/ska_low_mccs_spshw/tile/tile_device.py
+++ src/ska_low_mccs_spshw/tile/tile_device.py
@@ -269,7 +269,6 @@ class MccsTile(MccsBaseDevice[TileComponentManager]):
             "core_communication": "coreCommunicationStatus",
             "board_temperature": "boardTemperature",
             "rfi_count": "rfiCount",
-            "ddr_write_size": "ddr_write_size",
         }
 
         attribute_converters: dict[str, Any] = {
@@ -1294,7 +1293,7 @@ class MccsTile(MccsBaseDevice[TileComponentManager]):
 
         :return: ddr write size of a frame
         """
-        return self._attribute_state["ddr_write_size"].read()
+        return self.component_manager.ddr_write_size
 
     # @attribute(
     #     dtype="DevString",
@@ -5479,7 +5478,7 @@ class MccsTile(MccsBaseDevice[TileComponentManager]):
         :example:
 
         >>> dp = tango.DeviceProxy("mccs/tile/01")
-        >>> dp.command_inout("DisableStationBeamFlagging")
+        >>> dp.command_inout("DisableStationBeamFlagging")k
         """
         handler = self.get_command_object("DisableStationBeamFlagging")
         (return_code, message) = handler()

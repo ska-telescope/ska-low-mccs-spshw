0ab189c3531142973edf33bd9b0cc043d5ff0fd2
Alistair <abc@observatorysciences.co.uk>
2025-05-29 15:43:22 +0000

    SKB-928: Correct assumptions about configuration.

--- src/ska_low_mccs_spshw/tile/tile_device.py
+++ src/ska_low_mccs_spshw/tile/tile_device.py
@@ -330,14 +330,6 @@ class MccsTile(MccsBaseDevice[TileComponentManager]):
                     functools.partial(self.post_change_event, "ppsPresent"),
                     alarm_flag="LOW",
                 ),
-                "stationId": AttributeManager(
-                    functools.partial(self.post_change_event, "stationId"),
-                    initial_value=self.StationID,
-                ),
-                "logicalTileId": AttributeManager(
-                    functools.partial(self.post_change_event, "logicalTileId"),
-                    initial_value=self.TileId,
-                ),
                 "tileProgrammingState": AttributeManager(
                     functools.partial(self.post_change_event, "tileProgrammingState"),
                     initial_value=TpmStatus.UNKNOWN.pretty_name(),
@@ -1766,7 +1758,7 @@ class MccsTile(MccsBaseDevice[TileComponentManager]):
             "the 'TestConfig' property set as desired."
         )
 
-    @attribute(dtype="DevLong", abs_change=1)
+    @attribute(dtype="DevLong", abs_change=1, min_value=-1)
     def logicalTileId(self: MccsTile) -> int:
         """
         Return the logical tile id.

1d4c8453220518cdfabd19f4d0922386d740bfaf
Alistair <abc@observatorysciences.co.uk>
2025-06-09 14:52:25 +0000

    SKB-928: Refactor fixture

--- tests/functional/test_station_self_check.py
+++ tests/functional/test_station_self_check.py
@@ -49,32 +49,6 @@ def command_info_fixture() -> dict[str, Any]:
     return {}
 
 
-@pytest.fixture(name="sps_devices_trl_root")
-def sps_devices_trl_root_fixture() -> list[str]:
-    """
-    Fixture containing the trl root for all sps devices.
-
-    :returns: A list of trl strings.
-    """
-    tile_devices = [
-        tango.DeviceProxy(trl)
-        for trl in tango.Database().get_device_exported("low-mccs/tile/*")
-    ]
-    subrack_devices = [
-        tango.DeviceProxy(trl)
-        for trl in tango.Database().get_device_exported("low-mccs/subrack/*")
-    ]
-    daq_devices = [
-        tango.DeviceProxy(trl)
-        for trl in tango.Database().get_device_exported("low-mccs/daqreceiver/*")
-    ]
-    station_devices = [
-        tango.DeviceProxy(trl)
-        for trl in tango.Database().get_device_exported("low-mccs/spsstation/*")
-    ]
-    return tile_devices + subrack_devices + daq_devices + station_devices
-
-
 @scenario("features/station_self_check.feature", "Test SpsStation Self Check")
 def test_station_self_check(sps_devices_trl_root: list[str]) -> None:
     """

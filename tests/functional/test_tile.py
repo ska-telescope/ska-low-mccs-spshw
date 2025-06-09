f8ada2ea4c195b31c886d0f10167a80beca14393
Alistair <abc@observatorysciences.co.uk>
2025-06-09 15:58:02 +0000

    SKB-928: Fix TestWrapper

--- tests/functional/test_tile.py
+++ tests/functional/test_tile.py
@@ -16,6 +16,7 @@ from __future__ import annotations
 import time
 from typing import Any
 
+import numpy as np
 import pytest
 import tango
 from pytest_bdd import given, scenario, then, when
@@ -222,9 +223,9 @@ def tile_has_defined_synchronised_state(
     defined_state = {
         "logical_tile_id": 2,
         "station_id": 2,
-        "static_time_delays": [5] * 32,
-        "csp_rounding": [4] * 32,
-        "channeliser_rounding": [8] * 512,
+        "static_time_delays": np.array([5] * 32),
+        "csp_rounding": np.array([4] * 32),
+        "channeliser_rounding": np.array([8] * 512),
     }
     tw = TileWrapper(tile_device)
     tw.set_state(programming_state=6, **defined_state)
@@ -246,9 +247,9 @@ def tile_has_defined_initialised_state(
     defined_state = {
         "logical_tile_id": 2,
         "station_id": 2,
-        "static_time_delays": [5] * 32,
-        "csp_rounding": [4] * 32,
-        "channeliser_rounding": [8] * 512,
+        "static_time_delays": np.array([5] * 32),
+        "csp_rounding": np.array([4] * 32),
+        "channeliser_rounding": np.array([8] * 512),
     }
     tw = TileWrapper(tile_device)
     tw.set_state(

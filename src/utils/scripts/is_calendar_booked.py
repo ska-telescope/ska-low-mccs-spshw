0e3b4c17387bf8180e937b19a3c2c1210467d335
Alistair <abc@observatorysciences.co.uk>
2025-06-11 07:36:52 +0100

    THORN-214: Correct pipeline to allow_failure.

--- src/utils/scripts/is_calendar_booked.py
+++ src/utils/scripts/is_calendar_booked.py
@@ -5,7 +5,7 @@
 #
 # Distributed under the terms of the BSD 3-clause new license.
 # See LICENSE for more info.
-"""This subpackage contains tests of the ska-low-mccs project."""
+"""This file contains a script for checking if the calendar is booked."""
 
 
 import argparse

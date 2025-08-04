c537d72e70b0c81df19358accdea6d1660e0b8d6
George S. Chira <georgestefan.chira@gmail.com>
2025-08-04 17:45:29 +0100

    THORN-218: added vref back to test

--- tests/unit/tile/test_tile_device.py
+++ tests/unit/tile/test_tile_device.py
@@ -719,7 +719,6 @@ class TestMccsTile:
             "ddr_rd_cnt",
             "ddr_wr_cnt",
             "ddr_rd_dat_cnt",
-            "voltageVref2V5",
         ]
 
         all_excluded_attribute = (

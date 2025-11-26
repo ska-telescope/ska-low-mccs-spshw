e6fb4a3c48062cfdfb747d7cd183bf0210be8d0b
Alistair <abc@observatorysciences.co.uk>
2025-11-26 10:23:48 +0000

    SKB-1079: Add cleanup.

--- tests/functional/test_firmware_thresholds.py
+++ tests/functional/test_firmware_thresholds.py
@@ -15,7 +15,7 @@ from typing import Any, Generator
 import pytest
 import tango
 from pytest_bdd import given, scenario, then, when
-from ska_control_model import AdminMode
+from ska_control_model import AdminMode, SimulationMode
 from ska_tango_testing.mock.placeholders import Anything
 from ska_tango_testing.mock.tango import MockTangoEventCallbackGroup
 
@@ -108,11 +108,23 @@ def device_threshold_updated_fixture(
     tango.DeviceProxy(tile_device.adm_name()).restartserver()
     # Sleep to allow time for device to come up.
     time.sleep(6)
+    # Due to the way the TileSimulator if coupled to the lifetime of the device
+    # It is recreated on init_device. This means that is will be re-initialised
+    # from the subrack callback. This leads to a few more lookaheads
+    # UNKNOWN -> UNPROGRAMMED -> PROGRAMMED -> INITIALISED.
+    # When testing against hw the state is discovered directly
+    # UNKNOWN first hence lookahead == 2
+    lookahead = (
+        6
+        if int(tile_device.get_property("SimulationConfig")["SimulationConfig"][0])
+        == SimulationMode.TRUE
+        else 2
+    )
     AttributeWaiter(timeout=45).wait_for_value(
         tile_device,
         "tileProgrammingState",
         "Initialised",
-        lookahead=2,  # UNKNOWN first hence lookahead == 2
+        lookahead=lookahead,
     )
 
 
@@ -382,11 +394,23 @@ def check_for_configuration_missmatch(
     :param change_event_callbacks: a dictionary of callables to be used as
         tango change event callbacks.
     """
+    # Due to the way the TileSimulator if coupled to the lifetime of the device
+    # It is recreated on init_device. This means that is will be re-initialised
+    # from the subrack callback. This leads to a few more lookaheads
+    # UNKNOWN -> UNPROGRAMMED -> PROGRAMMED -> INITIALISED.
+    # When testing against hw the state is discovered directly
+    # UNKNOWN first hence lookahead == 2
+    lookahead = (
+        6
+        if int(tile_device.get_property("SimulationConfig")["SimulationConfig"][0])
+        == SimulationMode.TRUE
+        else 2
+    )
     AttributeWaiter(timeout=45).wait_for_value(
         tile_device,
         "tileProgrammingState",
         "Initialised",
-        lookahead=2,  # UNKNOWN first hence lookahead == 2
+        lookahead=lookahead,
     )
     assert tile_device.state() == tango.DevState.FAULT
 

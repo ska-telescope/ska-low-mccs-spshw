344886884946f437a123940843ec0876224f7174
Tom Moynihan <tdm@observatorysciences.co.uk>
2026-05-11 10:02:26 +0000

    SKB-1228: Add debug log

--- src/ska_low_mccs_spshw/station/tests/test_tile_beamformer.py
+++ src/ska_low_mccs_spshw/station/tests/test_tile_beamformer.py
@@ -90,14 +90,7 @@ class TestTileBeamformer(BaseDaqTest):
         self._ref_pol = randomiser.randrange(0, TileData.POLS_PER_ANTENNA, 1)
 
         self._start_freq = 156.25e6  # Hz
-        self._daq_send_count = 0
         super().__init__(component_manager, logger, tile_trls, subrack_trls, daq_trl)
-
-    def _configure_daq_for_send(self: TestTileBeamformer) -> None:
-        """Reconfigure DAQ with a fresh directory for this send."""
-        directory = f"beam_send_{self._daq_send_count}/"
-        self._daq_send_count += 1
-        self._configure_daq("BEAM_DATA", directory=directory)
 
     def _send_beam_data(self: TestTileBeamformer) -> None:
         """Send beam data to the DAQ."""
@@ -144,7 +137,6 @@ class TestTileBeamformer(BaseDaqTest):
             dtype="complex",
         )
         for antenna_no in range(TileData.ANTENNA_COUNT):
-            self._configure_daq_for_send()
             self._start_directory_watch()
             self.test_logger.debug(f"Sending beam data for {antenna_no=}")
             frequency = self._start_freq + (channel * TileData.CHANNEL_WIDTH)
@@ -179,7 +171,6 @@ class TestTileBeamformer(BaseDaqTest):
 
         :param channel: the channel to get the beam data for.
         """
-        self._configure_daq_for_send()
         self._start_directory_watch()
         self.test_logger.debug("Sending beam data for all antennas")
         frequency = self._start_freq + (channel * TileData.CHANNEL_WIDTH)
@@ -226,6 +217,10 @@ class TestTileBeamformer(BaseDaqTest):
                     )
             self.test_logger.debug(
                 f"Calibration coeffs for tile {tile_no} : {coeffs[tile_no]}"
+            )
+            self.test_logger.debug(
+                f"Beam value for tile {tile_no} before "
+                f"calibration: {single_input_data[tile_no]}"
             )
             self._load_calibration_coefficients(tile, channel, coeffs[tile_no])
 
@@ -346,6 +341,7 @@ class TestTileBeamformer(BaseDaqTest):
             start_time=start_time, channel_groups=None, duration=-1, scan_id=0
         )
         time.sleep(5)
+        self._configure_daq("BEAM_DATA")
         self._data_handler = BeamDataReceivedHandler(
             self.test_logger,
             len(self.tile_proxies),

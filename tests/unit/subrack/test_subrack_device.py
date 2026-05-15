d9576d6d07a755a5bcc342ae35fe0e54021a3b6f
George S. Chira <georgestefan.chira@gmail.com>
2026-05-15 18:46:38 +0100

    THORN-436: updated unit tests to remove deprecated longrunningcommand attributes in favour of new lrc attributes

--- tests/unit/subrack/test_subrack_device.py
+++ tests/unit/subrack/test_subrack_device.py
@@ -5,7 +5,6 @@
 #
 # Distributed under the terms of the BSD 3-clause new license.
 # See LICENSE for more info.
-# pylint: disable = too-many-lines
 """This module contains the tests of the subrack Tango device."""
 from __future__ import annotations
 
@@ -27,6 +26,7 @@ from ska_low_mccs_spshw.subrack import (
     SubrackSimulator,
 )
 from tests.harness import SpsTangoTestHarness, SpsTangoTestHarnessContext
+from tests.test_tools import LRCManager
 
 # TODO: Weird hang-at-garbage-collection bug
 gc.disable()
@@ -42,8 +42,9 @@ def change_event_callbacks_fixture() -> MockTangoEventCallbackGroup:
     """
     return MockTangoEventCallbackGroup(
         "state",
-        "command_status",
-        "command_result",
+        "lrc_executing",
+        "lrc_finished",
+        "lrc_queue",
         "healthState",
         "attribute",
         "tpmPresent",
@@ -341,45 +342,19 @@ def test_off_on(
     )
 
     # It's on, so let's turn it off.
-    subrack_device.subscribe_event(
-        "longRunningCommandStatus",
-        EventType.CHANGE_EVENT,
-        change_event_callbacks["command_status"],
+    subrack_lrc_manager = LRCManager(subrack_device, change_event_callbacks)
+    subrack_lrc_manager.run_command_with_checks(
+        "Off", expected_status=ResultCode.QUEUED
     )
-
-    change_event_callbacks["command_status"].assert_change_event(())
-
-    subrack_device.subscribe_event(
-        "longRunningCommandResult",
-        EventType.CHANGE_EVENT,
-        change_event_callbacks["command_result"],
-    )
-    change_event_callbacks["command_result"].assert_change_event(("", ""))
-
-    ([result_code], [off_command_id]) = subrack_device.off()
-    assert result_code == ResultCode.QUEUED
-
-    change_event_callbacks["command_status"].assert_change_event(
-        (off_command_id, "STAGING")
-    )
-    change_event_callbacks["command_status"].assert_change_event(
-        (off_command_id, "QUEUED")
-    )
-    change_event_callbacks["command_status"].assert_change_event(
-        (off_command_id, "IN_PROGRESS")
-    )
+    subrack_lrc_manager.assert_command_queued()
+    subrack_lrc_manager.assert_command_in_progress()
     change_event_callbacks["state"].assert_change_event(DevState.OFF)
-
     assert subrack_device.state() == DevState.OFF
 
-    change_event_callbacks["command_result"].assert_change_event(
-        (
-            off_command_id,
-            json.dumps([int(ResultCode.OK), "Command completed"]),
-        ),
-    )
-    change_event_callbacks["command_status"].assert_change_event(
-        (off_command_id, "COMPLETED")
+    subrack_lrc_manager.assert_command_finished(
+        status="COMPLETED",
+        result_code=ResultCode.OK,
+        result_message="Command completed",
     )
 
     change_event_callbacks["boardCurrent"].assert_change_event([])
@@ -387,8 +362,11 @@ def test_off_on(
         _ = subrack_device.boardCurrent
 
     # Okay, let's turn it back on again,
-    # but we can't be bothered tracking the command status this time.
-    _ = subrack_device.on()
+    subrack_lrc_manager.run_command_with_checks("On", expected_status=ResultCode.QUEUED)
+    subrack_lrc_manager.assert_command_finished(
+        status="COMPLETED",
+        result_code=ResultCode.OK,
+    )
 
     change_event_callbacks["state"].assert_change_event(DevState.UNKNOWN)
     change_event_callbacks["state"].assert_change_event(DevState.ON)
@@ -542,20 +520,7 @@ def test_monitoring_and_control(
     )
 
     # Now let's try a command
-    subrack_device.subscribe_event(
-        "longRunningCommandStatus",
-        EventType.CHANGE_EVENT,
-        change_event_callbacks["command_status"],
-    )
-
-    change_event_callbacks["command_status"].assert_change_event(())
-
-    subrack_device.subscribe_event(
-        "longRunningCommandResult",
-        EventType.CHANGE_EVENT,
-        change_event_callbacks["command_result"],
-    )
-    change_event_callbacks["command_result"].assert_change_event(("", ""))
+    subrack_lrc_manager = LRCManager(subrack_device, change_event_callbacks)
 
     tpm_present = list(subrack_device.tpmPresent)
     tpm_to_power = tpm_present.index(True) + 1
@@ -563,31 +528,19 @@ def test_monitoring_and_control(
     if power_state == PowerState.OFF:
         expected_power_state = PowerState.ON
 
-        ([result_code], [tpm_on_command_id]) = subrack_device.PowerOnTpm(tpm_to_power)
-        assert result_code == ResultCode.QUEUED
-
-        change_event_callbacks["command_status"].assert_change_event(
-            (tpm_on_command_id, "STAGING")
+        subrack_lrc_manager.run_command_with_checks(
+            "PowerOnTpm", tpm_to_power, ResultCode.QUEUED
         )
-        change_event_callbacks["command_status"].assert_change_event(
-            (tpm_on_command_id, "QUEUED")
-        )
-        change_event_callbacks["command_status"].assert_change_event(
-            (tpm_on_command_id, "IN_PROGRESS")
-        )
+        subrack_lrc_manager.assert_command_queued()
+        subrack_lrc_manager.assert_command_in_progress()
 
         change_event_callbacks[f"tpm{tpm_to_power}PowerState"].assert_change_event(
             PowerState.ON, lookahead=5, consume_nonmatches=True
         )
-
-        change_event_callbacks["command_result"].assert_change_event(
-            (
-                tpm_on_command_id,
-                json.dumps([int(ResultCode.OK), "Command completed."]),
-            ),
-        )
-        change_event_callbacks["command_status"].assert_change_event(
-            (tpm_on_command_id, "COMPLETED")
+        subrack_lrc_manager.assert_command_finished(
+            status="COMPLETED",
+            result_code=ResultCode.OK,
+            result_message="Command completed.",
         )
 
     _ = subrack_device.PowerDownTpms()

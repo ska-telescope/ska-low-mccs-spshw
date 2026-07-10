5154dccb9768e91a160f88b02a7f01779bd4c2cc
Alistair <abc@observatorysciences.co.uk>
2026-07-10 16:40:00 +0000

    THORN-656: Update CHANGELOG and bump major version

--- src/ska_low_mccs_spshw/version.py
+++ src/ska_low_mccs_spshw/version.py
@@ -9,7 +9,7 @@
 
 version_info = {
     "name": "ska_low_mccs_spshw",
-    "version": "13.0.0",
+    "version": "14.0.0",
     "description": (
         "Monitoring and control of SPS hardware as part of the MCCS subsystem"
     ),

442429ed1048bf464b9202c500e253a69b99a4d8
Alistair <abc@observatorysciences.co.uk>
2026-07-30 14:32:32 +0000

    THORN-680: Update SpsStation to report cached reads.

--- tests/unit/station/test_station_component_manager.py
+++ tests/unit/station/test_station_component_manager.py
@@ -295,6 +295,7 @@ def test_trigger_adc_equalisation(
     result: float,
     target_adc: float,
     bias: float,
+    mock_tile_device_proxies: list[unittest.mock.Mock],
 ) -> None:
     """
     Test the adc triggering equalisation.
@@ -304,13 +305,16 @@ def test_trigger_adc_equalisation(
     :param result: expected result after equalisation
     :param target_adc: the expected average power received by antennas in ADU units.
     :param bias: user specified bias.
+    :param mock_tile_device_proxies: a mock tile device proxy
+        that has been configured with the required subrack behaviours.
     """
     expected_adc = 16.0
     expected_preadu = 8.0
 
-    for proxy in communicating_station_component_manager._tile_proxies.values():
-        proxy._proxy.adcPower = [expected_adc] * 32  # type: ignore
-        proxy._proxy.preaduLevels = [expected_preadu] * 32  # type: ignore
+    for mock_tile in mock_tile_device_proxies:
+        mock_tile.adcPower = [expected_adc] * 32  # type: ignore
+        mock_tile.preaduLevels = [expected_preadu] * 32  # type: ignore
+        mock_tile.tileProgrammingState = "Synchronised"  # type: ignore
 
     # Assertion fails, the preadu levels may be empty or containing something
     # in a non deterministic way

4a1769198a7a6a267394e865bf5a25b88a690a80
Tom Moynihan <tdm@observatorysciences.co.uk>
2026-09-18 13:49:02 +0100

    THORN-714: Add debug log for successful poll

--- src/ska_low_mccs_spshw/prototype_subrack/prototype_subrack_device.py
+++ src/ska_low_mccs_spshw/prototype_subrack/prototype_subrack_device.py
@@ -206,6 +206,7 @@ class MccsPrototypeSubrack(SubrackAttributes, BaseInterface):
         self.component_on()
         self.component_no_fault()
         self.report_health(HealthState.OK, [])
+        self.logger.debug("Poll succeeded")
 
     def _poll_failed(self: MccsPrototypeSubrack, exception: Exception) -> None:
         """
